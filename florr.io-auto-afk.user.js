4fcabbd35e82628fab2b12ea9f3e90cc5cca2547
IceCang <107686384+IceCang@users.noreply.github.com>
2023-05-04 22:15:15 +0800

    Update florr.io-auto-afk.user.js

--- florr.io-auto-afk.user.js
+++ florr.io-auto-afk.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Florr.io Auto AFK
-// @namespace    https://github.com/IceCang
+// @namespace    http://tampermonkey.net/
 // @version      0.0.1
 // @description  Auto AFK in florr.io
 // @author       liucang (3060272052@qq.com)
