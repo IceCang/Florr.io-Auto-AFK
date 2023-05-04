04d4235abf0055651310bdae84315a8fabef8af3
IceCang <107686384+IceCang@users.noreply.github.com>
2023-05-04 22:05:43 +0800

    Create florr.io-auto-afk-0.0.1.js

---
+++ florr.io-auto-afk-0.0.1.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://florr.io/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=florr.io
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
